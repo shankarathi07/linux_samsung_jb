0ee590485e97bf35081c65d8df3031a2a278353d
Ezekeel <notezekeel@googlemail.com>
2011-12-17 08:26:36 +0100

    Added Touch Wake version 1.
    Conflicts:
    
    	drivers/input/keyboard/cypress-touchkey.c
    	drivers/input/touchscreen/mxt224.c
    	drivers/misc/Kconfig
    	drivers/misc/Makefile

--- drivers/input/keyboard/cypress-touchkey.c
+++ drivers/input/keyboard/cypress-touchkey.c
@@ -35,6 +35,9 @@
 
 #ifdef CONFIG_BLD
 #include <linux/bld.h>
+
+#ifdef CONFIG_TOUCH_WAKE
+#include <linux/touch_wake.h>
 #endif
 
 #define SCANCODE_MASK		0x07
@@ -63,12 +66,17 @@ struct cypress_touchkey_devdata {
 	bool has_legacy_keycode;
 };
 
+ 
 #ifdef CONFIG_GENERIC_BLN
 static struct cypress_touchkey_devdata *blndevdata;
 #endif
 
 #ifdef CONFIG_BLD
 static struct cypress_touchkey_devdata *blddevdata;
+
+#ifdef CONFIG_TOUCH_WAKE
+static struct cypress_touchkey_devdata *touchwakedevdata;
+ ... .
 #endif
 
 static int i2c_touchkey_read_byte(struct cypress_touchkey_devdata *devdata,
@@ -185,6 +193,7 @@ static irqreturn_t touchkey_interrupt_thread(int irq, void *touchkey_devdata)
 				"range\n", __func__);
 			goto err;
 		}
+ 
 		input_report_key(devdata->input_dev,
 			devdata->pdata->keycode[scancode],
 			!(data & UPDOWN_EVENT_MASK));
@@ -194,11 +203,39 @@ static irqreturn_t touchkey_interrupt_thread(int irq, void *touchkey_devdata)
 		    {
 #ifdef CONFIG_BLD			
 			touchkey_pressed();
+
+
+#ifdef CONFIG_TOUCH_WAKE
+		if (!device_is_suspended())
+#endif
+		    {
+			input_report_key(devdata->input_dev,
+					 devdata->pdata->keycode[scancode],
+					 !(data & UPDOWN_EVENT_MASK));
+		    }
+#if defined(CONFIG_TOUCH_WAKE) || defined(CONFIG_BLD)
+		if (!(data & UPDOWN_EVENT_MASK))
+		    {
+#ifdef CONFIG_TOUCH_WAKE
+			touch_press();
+ ... .
 #endif
 		    }
 #endif
 	} else {
+#ifdef CONFIG_TOUCH_WAKE
+		if (!device_is_suspended())
+#endif
+		    {
+			for (i = 0; i < devdata->pdata->keycode_cnt; i++)
+			    input_report_key(devdata->input_dev,
+					     devdata->pdata->keycode[i],
+					     !!(data & (1U << i)));
+		    }
+
+#if defined(CONFIG_TOUCH_WAKE) || defined(CONFIG_BLD)
 		for (i = 0; i < devdata->pdata->keycode_cnt; i++)
+ 
 			input_report_key(devdata->input_dev,
 				devdata->pdata->keycode[i],
 				!!(data & (1U << i)));
@@ -210,6 +247,13 @@ static irqreturn_t touchkey_interrupt_thread(int irq, void *touchkey_devdata)
 			    {
 #ifdef CONFIG_BLD			
 				touchkey_pressed();
+
+		    {
+			if(!!(data & (1U << i)))
+			    {
+#ifdef CONFIG_TOUCH_WAKE
+				touch_press();
+ ... .
 #endif
 				break;
 			    }
@@ -241,6 +285,9 @@ static void cypress_touchkey_early_suspend(struct early_suspend *h)
 	struct cypress_touchkey_devdata *devdata =
 		container_of(h, struct cypress_touchkey_devdata, early_suspend);
 
+#ifdef CONFIG_TOUCH_WAKE
+	i2c_touchkey_write_byte(devdata, devdata->backlight_off);
+#else
 	devdata->is_powering_on = true;
 
 	if (unlikely(devdata->is_dead))
@@ -262,6 +309,7 @@ static void cypress_touchkey_early_suspend(struct early_suspend *h)
 #endif
 
 	all_keys_up(devdata);
+#endif
 }
 
 static void cypress_touchkey_early_resume(struct early_suspend *h)
@@ -269,6 +317,9 @@ static void cypress_touchkey_early_resume(struct early_suspend *h)
 	struct cypress_touchkey_devdata *devdata =
 		container_of(h, struct cypress_touchkey_devdata, early_suspend);
 
+#ifdef CONFIG_TOUCH_WAKE
+	i2c_touchkey_write_byte(devdata, devdata->backlight_on);
+#else
 	devdata->pdata->touchkey_onoff(TOUCHKEY_ON);
 	if (i2c_touchkey_write_byte(devdata, devdata->backlight_on)) {
 		devdata->is_dead = true;
@@ -280,6 +331,7 @@ static void cypress_touchkey_early_resume(struct early_suspend *h)
 	devdata->is_dead = false;
 	enable_irq(devdata->client->irq);
 	devdata->is_powering_on = false;
+#endif
 }
 #endif
 
@@ -380,6 +432,7 @@ done:
 	return 0;
 }
 
+ 
 #ifdef CONFIG_GENERIC_BLN
 static void enable_touchkey_backlights(void){
        i2c_touchkey_write_byte(blndevdata, blndevdata->backlight_on);
@@ -467,6 +520,63 @@ static struct bld_implementation cypress_touchkey_bld =
 	.disable = cypress_touchkey_bld_disable,
     };
 
+
+#ifdef CONFIG_TOUCH_WAKE
+static void cypress_touchwake_disable(void)
+{
+    touchwakedevdata->is_powering_on = true;
+
+    if (unlikely(touchwakedevdata->is_dead))
+	return;
+
+    disable_irq(touchwakedevdata->client->irq);
+
+#ifdef CONFIG_GENERIC_BLN
+    /*
+     * Disallow powering off the touchkey controller
+     * while a led notification is ongoing
+     */
+    if(bln_is_ongoing())
+	{
+	    cypress_touchkey_enable_led_notification();
+	}
+    else
+	{
+	    touchwakedevdata->pdata->touchkey_onoff(TOUCHKEY_OFF);
+	    touchwakedevdata->pdata->touchkey_sleep_onoff(TOUCHKEY_OFF);
+	}
+#else
+    touchwakedevdata->pdata->touchkey_onoff(TOUCHKEY_OFF);
+#endif
+
+    all_keys_up(touchwakedevdata);
+
+    return;
+}
+
+static void cypress_touchwake_enable(void)
+{
+    touchwakedevdata->pdata->touchkey_onoff(TOUCHKEY_ON);
+    if (i2c_touchkey_write_byte(touchwakedevdata, touchwakedevdata->backlight_on)) {
+	touchwakedevdata->is_dead = true;
+	touchwakedevdata->pdata->touchkey_onoff(TOUCHKEY_OFF);
+	dev_err(&touchwakedevdata->client->dev, "%s: touch keypad not responding"
+		" to commands, disabling\n", __func__);
+	return;
+    }
+    touchwakedevdata->is_dead = false;
+    enable_irq(touchwakedevdata->client->irq);
+    touchwakedevdata->is_powering_on = false;
+
+    return;
+}
+
+static struct touchwake_implementation cypress_touchwake = 
+    {
+	.enable = cypress_touchwake_enable,
+	.disable = cypress_touchwake_disable,
+    };
+ ... .
 #endif
 
 static int cypress_touchkey_probe(struct i2c_client *client,
@@ -568,6 +678,7 @@ static int cypress_touchkey_probe(struct i2c_client *client,
 
 	devdata->is_powering_on = false;
 
+ 
 #ifdef CONFIG_GENERIC_BLN
 	blndevdata = devdata;
 	register_bln_implementation(&cypress_touchkey_bln);
@@ -576,6 +687,11 @@ static int cypress_touchkey_probe(struct i2c_client *client,
 #ifdef CONFIG_BLD
 	blddevdata = devdata;
 	register_bld_implementation(&cypress_touchkey_bld);
+
+#ifdef CONFIG_TOUCH_WAKE
+	touchwakedevdata = devdata;
+	register_touchwake_implementation(&cypress_touchwake);
+ ... .
 #endif
 
 	return 0;
