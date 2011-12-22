72b0b5e849539a12334b7f693048ff1e763eae05
Ezekeel <notezekeel@googlemail.com>
2011-12-22 15:37:00 +0100

    Using 'cpufreq_frequency_table_target' to determine the previous index fails when the current frequency is lower than the minimum or larger than the maximum frequency (which can happen when the frequency limits are changed). As a consequence changes in the PLL frequency and bus speed are not properly detected which causes instablilites.
    Conflicts:
    
    	arch/arm/mach-s5pv210/cpufreq.c

--- arch/arm/mach-s5pv210/cpufreq.c
+++ arch/arm/mach-s5pv210/cpufreq.c
@@ -196,7 +196,7 @@ static int s5pv210_target(struct cpufreq_policy *policy,
 			  unsigned int relation)
 {
 	unsigned long reg;
-	unsigned int index, priv_index;
+	unsigned int index;
 	unsigned int pll_changing = 0;
 	unsigned int bus_speed_changing = 0;
 	unsigned int arm_volt, int_volt;
@@ -232,13 +232,6 @@ static int s5pv210_target(struct cpufreq_policy *policy,
 	if (freqs.new == freqs.old)
 		goto out;
 
-	/* Finding current running level index */
-	if (cpufreq_frequency_table_target(policy, s5pv210_freq_table,
-					   freqs.old, relation, &priv_index)) {
-		ret = -EINVAL;
-		goto out;
-	}
-
 	arm_volt = dvs_conf[index].arm_volt;
 	int_volt = dvs_conf[index].int_volt;
 
@@ -260,11 +253,11 @@ static int s5pv210_target(struct cpufreq_policy *policy,
 	cpufreq_notify_transition(&freqs, CPUFREQ_PRECHANGE);
 
 	/* Check if there need to change PLL */
-	if ((index == L0) || (priv_index == L0))
+	if ((index <= L0) || (freqs.old >= s5pv210_freq_table[L0].frequency))
 		pll_changing = 1;
 
 	/* Check if there need to change System bus clock */
-	if ((index == L4) || (priv_index == L4))
+	if ((index == L4) || (freqs.old == s5pv210_freq_table[L4].frequency))
 		bus_speed_changing = 1;
 
 	if (bus_speed_changing) {
