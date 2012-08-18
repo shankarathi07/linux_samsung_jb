a69bc026d821f065398b13543e9ff16fa2b29b0c
Shankar <mathkid95@gmail.com>
2012-08-17 19:23:21 -0500

    Revert "Merge commit 'v3.0.38'"
    This reverts commit ee487218cabb6d7b51ca0812a3c4f33fc8d38d2b, reversing
    changes made to 3b0c5d2887fca99cab7dd506817b1049d38198a1.

--- arch/x86/xen/enlighten.c
+++ arch/x86/xen/enlighten.c
@@ -62,7 +62,6 @@
 #include <asm/reboot.h>
 #include <asm/stackprotector.h>
 #include <asm/hypervisor.h>
-#include <asm/pci_x86.h>
 
 #include "xen-ops.h"
 #include "mmu.h"
@@ -198,9 +197,6 @@ static void __init xen_banner(void)
 	       xen_feature(XENFEAT_mmu_pt_update_preserve_ad) ? " (preserve-AD)" : "");
 }
 
-#define CPUID_THERM_POWER_LEAF 6
-#define APERFMPERF_PRESENT 0
-
 static __read_mostly unsigned int cpuid_leaf1_edx_mask = ~0;
 static __read_mostly unsigned int cpuid_leaf1_ecx_mask = ~0;
 
@@ -219,11 +215,6 @@ static void xen_cpuid(unsigned int *ax, unsigned int *bx,
 	case 1:
 		maskecx = cpuid_leaf1_ecx_mask;
 		maskedx = cpuid_leaf1_edx_mask;
-		break;
-
-	case CPUID_THERM_POWER_LEAF:
-		/* Disabling APERFMPERF for kernel usage */
-		maskecx = ~(1 << APERFMPERF_PRESENT);
 		break;
 
 	case 0xb:
@@ -1268,10 +1259,8 @@ asmlinkage void __init xen_start_kernel(void)
 		/* Make sure ACS will be enabled */
 		pci_request_acs();
 	}
-#ifdef CONFIG_PCI
-	/* PCI BIOS service won't work from a PV guest. */
-	pci_probe &= ~PCI_PROBE_BIOS;
-#endif
+		
+
 	xen_raw_console_write("about to get started...\n");
 
 	xen_setup_runstate_info(0);
